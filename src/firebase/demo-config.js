6fd584b68ec3f3d1e6ba74a75424f10ffbba4122
Vishal_Bait <vishalbait01@gmail.com>
2020-08-20 09:19:53 +0530

    Add firebase, react,  react-bootstrap
    1. Add firebase config

---
+++ src/firebase/demo-config.js
@@ -0,0 +1,20 @@
+/* 
+import * as firebase from "firebase/app";
+import "firebase/storage";
+import "firebase/firestore";
+
+var app = firebase.initializeApp({
+  apiKey: "<your-api-key>",
+  authDomain: "<your-auth-domain>",
+  databaseURL: "<your-database-url>",
+  projectId: "<your-cloud-firestore-project>",
+  storageBucket: "<your-storage-bucket>",
+  messagingSenderId: "<your-sender-id>",
+});
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+
+export { projectFirestore, projectStorage };
+
+*/
