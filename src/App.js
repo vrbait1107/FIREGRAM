c0395deccd5c195abf47a25916d249f298aa4fed
Vishal_Bait <vishalbait01@gmail.com>
2020-08-21 11:28:44 +0530

    Add modal component 1. After clicking on image modal popups

--- src/App.js
+++ src/App.js
@@ -2,17 +2,27 @@ import React from "react";
 import Title from "./comps/Title";
 import UploadForm from "./comps/UploadForm";
 import ImageGrid from "./comps/ImageGrid";
+import ModalView from "./comps/ModalView";
 import { Container, Row } from "react-bootstrap";
+import { useState } from "react";
 
 function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [show, setShow] = useState(false);
+
   return (
     <div className="App">
       <Title />
       <UploadForm />
       <Container className="mt-3">
         <Row>
-          <ImageGrid />
+          <ImageGrid setShow={setShow} setSelectedImage={setSelectedImage} />
         </Row>
+        <ModalView
+          show={show}
+          setShow={setShow}
+          selectedImage={selectedImage}
+        />
       </Container>
     </div>
   );
--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -2,16 +2,19 @@ import React from "react";
 import useFirestore from "../hooks/useFirestore";
 import { Col } from "react-bootstrap";
 
-const ImageGrid = () => {
+const ImageGrid = (props) => {
   const { docs } = useFirestore("images");
 
-  console.log(docs);
+  const handleModal = (image) => {
+    props.setShow(true);
+    props.setSelectedImage(image);
+  };
 
   return (
     <React.Fragment>
       {docs &&
         docs.map((doc) => (
-          <Col md={4}>
+          <Col md={4} key={doc.id} onClick={() => handleModal(doc.urlValue)}>
             <img src={doc.urlValue} alt="images" className="img-fluid" />
           </Col>
         ))}
